6a4e1283f81d79721f40f36605a6b129e7759253
Dogsky <TaichiSky@users.noreply.github.com>
2018-08-22 23:05:26 +0800

    Update helloWorld.go

--- 1_helloWorld/helloWorld.go
+++ 1_helloWorld/helloWorld.go
@@ -1,6 +1,14 @@
 package main
 
 func main() {
-	greet := "hello,go!"
-	println("I saying:",greet)
+	greet1 := "Hello,go!"
+	println("I saying:",greet1)
+	
+	greet2 := takeGreets()
+	println(greet2())
+}
+func takeGreets() func() string {
+	return func() string {
+		return "Hello go!"
+	}
 }
